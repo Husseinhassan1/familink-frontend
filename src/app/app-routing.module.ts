c9f63c5e6c2d5fe70c8b939aa90b529a9cc669ae
FukiVeliki <127675594+FukiVeliki@users.noreply.github.com>
2023-04-04 14:13:14 +0200

    Revert "Added: Admin Dashboard"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,17 @@ import { PostFormComponent } from './post-form/post-form.component';
 import { PostPreviewComponent } from './post-preview/post-preview.component';
 import {LoginComponent} from "./login/login.component";
 import {SignupComponent} from "./signup/signup.component";
-import {LoginResolver} from "./resolvers/login.resolver";
-import {SignupResolver} from "./resolvers/signup.resolver";
 import {HomeComponent} from "./home/home.component";
 import {UserprofileComponent} from "./userprofile/userprofile.component";
 import {FollowinglistComponent} from "./followinglist/followinglist.component";
-import {AdminComponent} from "./admin/admin.component";
+import {LoginResolver} from "./resolvers/login.resolver";
+import {SignupResolver} from "./resolvers/signup.resolver";
 //import {UserProfileResolve} from "./resolvers/userprofile.resolver";
 
 const routes: Routes = [
 
   { path: 'post-form', component: PostFormComponent },
   { path: 'post-preview', component: PostPreviewComponent },
-
   {
 
     path: '',
@@ -42,15 +40,18 @@ const routes: Routes = [
   {
        path: 'userprofile',
       component:UserprofileComponent,
-  },
-  {
+  //   resolve: {
+  //     UserProfileResolve
+  // }
+    },
+    {
        path: 'userprofile/followinglist',
        component:FollowinglistComponent,
-  },
-  {
-    path: 'admin',
-    component:AdminComponent,
-  }
+    }
+  // {
+  //   path: 'home',
+  //   component:HomeComponent,
+  // }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { PostFormComponent } from './post-form/post-form.component';
 import { PostPreviewComponent } from './post-preview/post-preview.component';
+
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+
 import { HomeComponent } from './home/home.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { UserprofileComponent } from './userprofile/userprofile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { FollowinglistComponent } from './followinglist/followinglist.component';
-import {AdminComponent} from "./admin/admin.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignupComponent,
-    UserprofileComponent,
-    FollowinglistComponent,
+  LoginComponent,
+
+      SignupComponent,
+
+
+      SignupComponent,
+      UserprofileComponent,
+      FollowinglistComponent,
+
+      SignupComponent,
     PostFormComponent,
-    PostPreviewComponent,
-    HomeComponent,
-    AdminComponent,
+    PostPreviewComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +47,6 @@ import {AdminComponent} from "./admin/admin.component";
     BrowserAnimationsModule,
     MatMenuModule,
     MatIconModule,
-    MatButtonModule,
     MatButtonModule
   ],
 
