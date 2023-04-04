bd46f1177d4a85dd76066e0140a04c87240c9084
Husein2001 <huseinhasan2001@gmail.com>
2023-04-04 14:06:44 +0200

    Added: Admin Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { PostFormComponent } from './post-form/post-form.component';
 import { PostPreviewComponent } from './post-preview/post-preview.component';
-
-const routes: Routes = [
-  { path: '', redirectTo: '/post-form', pathMatch: 'full' },
-  { path: 'post-form', component: PostFormComponent },
-  { path: 'post-preview', component: PostPreviewComponent }
-];
-
 import {LoginComponent} from "./login/login.component";
 import {SignupComponent} from "./signup/signup.component";
 import {LoginResolver} from "./resolvers/login.resolver";
 import {SignupResolver} from "./resolvers/signup.resolver";
-
 import {HomeComponent} from "./home/home.component";
-
-
 import {UserprofileComponent} from "./userprofile/userprofile.component";
 import {FollowinglistComponent} from "./followinglist/followinglist.component";
-//import {UserProfileResolve} from "./resolvers/userprofile.resolver";
+import {AdminComponent} from "./admin/admin.component";
 
 const routes: Routes = [
+
+  {
+    path: '', redirectTo: '/post-form',
+    pathMatch: 'full'
+  },
+  {
+    path: 'post-form',
+    component: PostFormComponent
+  },
+  {
+    path: 'post-preview',
+    component: PostPreviewComponent
+  },
   {
     path: '',
     component: LoginComponent,
@@ -44,21 +46,18 @@ const routes: Routes = [
     component:HomeComponent,
   },
 
-
+  {
        path: 'userprofile',
       component:UserprofileComponent,
-  //   resolve: {
-  //     UserProfileResolve
-  // }
-    },
-    {
+  },
+  {
        path: 'userprofile/followinglist',
        component:FollowinglistComponent,
-    }
-  // {
-  //   path: 'home',
-  //   component:HomeComponent,
-  // }
+  },
+  {
+    path: 'admin',
+    component:AdminComponent,
+  }
 
 ];
 
