564f6a83ec28e2a6d9010109ffc76184502d510f
FukiVeliki <fuad.ljajic@stu.ssst.edu.ba>
2023-06-14 18:16:13 +0200

    Modified the service

--- src/app/services/userprofile.service.ts
+++ src/app/services/userprofile.service.ts
@@ -1,24 +1,37 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserprofileService {
+export class UserProfileService {
+  private apiUrl = 'your_api_endpoint'; // Replace with your API endpoint
 
- // constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-//  getUser(id:number): Observable<UserProfile> {
-  //go to backend and get data
- // return this.http.get<UserProfile>('https://localhost:8080/userprofile/'+ id)
-//  return of({
-// id: 5,
- // UserName: 'Muhammad Ali🥊';
- // });
+  getPhotos(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/photos`);
+  }
 
- // }
+  getFollowersCount(): Observable<number> {
+    return this.http.get<number>(`${this.apiUrl}/followers/count`);
+  }
 
+  getFollowingCount(): Observable<number> {
+    return this.http.get<number>(`${this.apiUrl}/following/count`);
+  }
 
+  getPostsCount(): Observable<number> {
+    return this.http.get<number>(`${this.apiUrl}/posts/count`);
+  }
 
+  getUserName(): Observable<string> {
+    return this.http.get<string>(`${this.apiUrl}/user/name`);
+  }
+
+  getUserDescription(): Observable<string> {
+    return this.http.get<string>(`${this.apiUrl}/user/description`);
+  }
 
 }
